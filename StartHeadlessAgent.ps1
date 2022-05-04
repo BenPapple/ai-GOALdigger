f09cd18f3e18642810ee9cc2a238de38b30cdb06
BenPapple <benjamingeweke@googlemail.com>
2022-05-04 11:13:30 +0200

    Update PowerShell script

--- StartHeadlessAgent.ps1
+++ StartHeadlessAgent.ps1
@@ -1,4 +1,4 @@
 #Write-Host "Starte Headless GOALdigger Agent"
-Start-Process -FilePath java -ArgumentList '-jar runtime-2.2.0.jar GOALDIGGER.mas2g'
+Start-Process -FilePath java -ArgumentList '-jar runtime-2.2.0.jar .\goaldiggersrc\GOALDIGGER.mas2g'
 
 
