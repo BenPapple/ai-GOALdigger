ccb70b6e1017cb04fbdd5ba69b49408018113902
BenPapple <benjamingeweke@googlemail.com>
2022-05-16 08:51:09 +0200

    Fix multiple comments and movement bugs
    Comments and indentations multiple files.
    Fixed some rotate bugs and movement with block.

--- goaldiggersrc/goaldiggerProlog.pl
+++ goaldiggersrc/goaldiggerProlog.pl
@@ -7,6 +7,7 @@
 :- dynamic agentAt/2, thing/4, randomAffinity/1.
 :- dynamic targetMd/2, nMd/1, sMd/1, wMd/1, eMd/1, executeManhattan/1. % Variables for Manhatten Distance 
 :- dynamic haveBlockAttached/2. 
+:- dynamic haveDispenserDelivery/2. % switch dispenser delivered block
 
 % Transform XY coordinates concerning direction D nswe
 transformXYD(n, X1, Y1, X2, Y2) :- X2 = X1, Y2 is Y1 - 1.
@@ -29,11 +30,20 @@ random90Direction(Affini, AltDir) :- random_between(0, 1, RandD),
 % get random role
 randomRole(Role) :- random_between(0, 1, RD), numbertoRoles(RD, Role).
 
+% get random rotate direction
+randomRotate(Dir) :- random_between(0, 1, R),
+			integerToRotate(R, Dir).
+
 % Calculate distance XY coordinates concerning target targetMd
 calculateXYMd(X1, Y1, X2, Y2, Md) :- Md is abs(X1 - X2) + abs(Y1 - Y2).
+
+% calculate minus or plus 1
 calculateMinusOne(A1, A2) :- A2 is (A1 - 1).
 calculatePlusOne(B1, B2) :- B2 is (B1 + 1).
 
+% helper function integer to rotate
+integerToRotate(0, cw).
+integerToRotate(1, ccw).
 
 % helper function random to direction
 integerToDirection(0, n).
@@ -67,6 +77,10 @@ oppositeDirection(s, n).
 oppositeDirection(w, e).
 oppositeDirection(e, w).
 
+% helper function direction to opposite rotate
+oppositeRotate(cw, ccw).
+oppositeRotate(ccw, cw).
+
 % helper function 2 directions to rotation
 rotateAgainstAffinity(n, n, cw).
 rotateAgainstAffinity(n, w, ccw).
@@ -90,4 +104,14 @@ rotateToDirection(w, cw, n).
 rotateToDirection(w, ccw, s).
 rotateToDirection(e, cw, s).
 rotateToDirection(e, ccw, n).
+
+% helper function rotation to coordinate nswe
+rotateToCoord(n, cw, 1, 0).
+rotateToCoord(n, ccw, -1, 0).
+rotateToCoord(s, cw, -1, 0).
+rotateToCoord(s, ccw, 1, 0).
+rotateToCoord(w, cw, 0, -1).
+rotateToCoord(w, ccw, 0, 1).
+rotateToCoord(e, cw, 0, 1).
+rotateToCoord(e, ccw, 0, -1).
 
